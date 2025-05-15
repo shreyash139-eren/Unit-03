dd9f243df9b04aaf2e3cfcae70628376fbdf16d8
Kumar Shreyash <shreyash.sinha35@gmail.com>
2025-05-16 02:39:15 +0530

    e

--- EventPropagationExploration.js
+++ EventPropagationExploration.js
@@ -1,23 +1,20 @@
 document.getElementById("parent").addEventListener("click",()=>{
-    alert("Parent Div Clicked!")
-})
-document.getElementById("parBtn").addEventListener("click",()=>{
-    alert("Parent Button clicked!")
-})
-
-
+    alert("Parent Div Clicked!-Capturing")
+},true)
 document.getElementById("child").addEventListener("click",()=>{
-    alert("Child Div Clicked!")
+    alert("Child Div Clicked!-Capturing ")
+},true)
+document.getElementById("gchild").addEventListener("click",()=>{
+    alert("GrandChild Div Clicked!-Capturing")
+},true)
+
+document.getElementById("parBtn").addEventListener("click",()=>{
+    alert("Parent Button clicked!-Bubbling")
 })
 document.getElementById("chilBtn").addEventListener("click",()=>{
-    alert("Child Button clicked!")
+    alert("Child Button clicked!-Bubbling")
 })
-
-
-document.getElementById("gchild").addEventListener("click",()=>{
-    alert("GrandChild Div Clicked!")
-})
-document.getElementById("gcBtn").addEventListener("click",()=>{
-    event.stopPropagation()
-    alert("GrandChild Button clicked!")
+document.getElementById("gcBtn").addEventListener("click",(e)=>{
+    e.stopPropagation
+    alert("GrandChild Button clicked!-Bubbling")
 })
